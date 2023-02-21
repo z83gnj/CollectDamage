12280d0575bb6c39225627cf3e52c89571c39f5c
Akos Matiny <akos.matiny@gmail.com>
2023-02-21 20:23:31 +0100

    Create ASCII file

---
+++ Create_ASCII/asc_gen.py
@@ -0,0 +1,44 @@
+
+
+
+def create_list (min, max, inc):
+    """
+    Create a list based on the min, max and the increment values.
+    The min and max values is included.
+    """
+    pass
+
+def read_list (filename):
+    """
+    Read a list from the given file.
+    """
+    pass
+
+def create_asc (values):
+    """
+    Create the ASCII timehistory file
+    """
+
+    pass
+
+
+
+def main ():
+    create_asc()
+
+
+'''
+#open text file
+text_file = open("data.txt", "w")
+
+torque = 1200
+#write string to file
+first_line = "Python Tutorial by TutorialKart.\n"
+second_line = "This is a new line"
+third_line = "Torque: "+ str(torque)
+
+text_file.write(first_line + second_line + third_line)
+ 
+#close file
+text_file.close()
+'''
